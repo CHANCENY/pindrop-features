0d14fbf4f84f25dbac1f1b51733d5a5ca9b2c6d0
CHANCENY <nyasuluchance6@gmail.com>
2026-09-02 15:49:13 +0530

    okay

--- ffmpeg_worker/api/SongsApiStandard.php
+++ ffmpeg_worker/api/SongsApiStandard.php
@@ -2,6 +2,7 @@
 
 namespace Simp\Pindrop\Modules\ffmpeg_worker\api;
 
+use InvalidArgumentException;
 use RuntimeException;
 use Simp\Pindrop\Modules\ffmpeg_worker\binaries\Binary;
 
@@ -37,5 +38,29 @@ class SongsApiStandard
 
         return $metadata;
 
+    }
+
+    public function extractAudioCoverImage(string $audioFile, string $outputFile): ?string
+    {
+
+        if (!is_file($audioFile)) {
+            throw new InvalidArgumentException(
+                "Audio file does not exist: {$audioFile}"
+            );
+        }
+
+        $command = sprintf(
+            $this->binary->getFFmpeg() . ' -y -v error -i %s -map 0:v:0 -frames:v 1 %s 2>&1',
+            escapeshellarg($audioFile),
+            escapeshellarg($outputFile)
+        );
+
+        exec($command, $output, $exitCode);
+
+        if ($exitCode !== 0 || !is_file($outputFile)) {
+            return null;
+        }
+
+        return $outputFile;
     }
 }
